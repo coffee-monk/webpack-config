6be3ef7e94dd93f37442bd0ac37cff43b789b53b
coffee-monk <coffeemonk@hotmail.com>
2021-05-23 14:56:42 -0400

    images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,21 @@
 import "../styles/index.scss";
-import Recipes from "./Recipes";
+import Recipes from "./Recipes.jsx";
+import sword from "../images/swc-sword.jpg";
+import swordSvg from "../images/sword.svg";
 
 const App = () => {
   return (
-    <div>
+    <>
       <section className="hero"></section>
       <main>
         <section>
           <h1>Oh Hai, React</h1>
         </section>
+        <img src={sword} alt="sword" width="250" />
+        <img src={swordSvg} alt="sword" width="250" />
+        <Recipes />
       </main>
-      <Recipes />
-    </div>
+    </>
   );
 };
 
