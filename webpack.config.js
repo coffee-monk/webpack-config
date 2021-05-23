c3a2110aa0e89f85119b6171a7b7ff16a759052e
coffee-monk <coffeemonk@hotmail.com>
2021-05-22 21:43:10 -0400

    css/sass loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,30 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 let mode = "development";
+let target = "web";
 
 if (process.env.NODE_ENV === "production") {
   mode = "production";
+  target = "browserslist";
 }
 
 module.exports = {
   mode: mode,
+  target: target,
 
   module: {
     rules: [
       {
-        test: /|.js$/,
+        test: /\.s?css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
@@ -19,8 +33,11 @@ module.exports = {
     ],
   },
 
+  plugins: [new MiniCssExtractPlugin()],
+
   devtool: "source-map",
   devServer: {
     contentBase: "./dist",
+    hot: true,
   },
 };
